f53e09b466f72a9ac37acbd4c35a68acbc7636d6
David SANTOS <milleunium300@hotmail.com>
2022-01-25 20:51:11 +0100

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
       #READ all -----------------------------
- 
-  # LISTS ------------------------------------
+ root to: 'lists#index'
+   # LISTS ------------------------------------
   resources :lists, only: [:index, :show, :create, :new] do
     resources :bookmarks, only: [:new, :create, :delete] # reviews est dans restaurant
   end
