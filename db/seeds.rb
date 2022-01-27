cc494d23456c23b029f4edbcf688f77f4554dd7e
David SANTOS <milleunium300@hotmail.com>
2022-01-27 20:52:33 +0100

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require "open-uri"
+
 Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
 Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
 Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
 Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
-List.create(name: "Ma première liste!")
+list1 = List.create(name: "Ma première liste!")
 List.create(name: "Ma deuxieme liste!")
 List.create(name: "Ma troisieme liste!")
+
+
+
+file = URI.open('https://cap.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2Fcap.2F2018.2F04.2F24.2Fc63f06c3-4035-4064-968c-5c265655cad4.2Ejpeg/1200x630/background-color/ffffff/quality/70/cr/wqkgRGF2aWQgU2xhdGVyIC8gV2lraW1lZGlhIENvbW1vbnMgLyBDQVBJVEFM/selfie-monkey-le-verdict-ahurissant-qui-refuse-daccorder-des-droits-dauteur-a-un-singe-1284699.jpg')
+list1.photo.attach(io: file, filename: 'first_list.jpg', content_type: 'image/jpg')
